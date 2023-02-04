c3187aae1982683e74905720d00ff9803defc16d
gianmarco <mar.oprisabenavides@stud.uniroma3.it>
2023-02-04 13:03:55 +0100

    primo commit

---
+++ src/main/java/it/uniroma3/siw/siwprogettocatering/SiwProgettoCateringApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwprogettocatering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProgettoCateringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProgettoCateringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/siwprogettocatering/SiwProgettoCateringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwprogettocatering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwProgettoCateringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
